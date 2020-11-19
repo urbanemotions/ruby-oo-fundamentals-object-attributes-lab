350579bd8ae3f38a97268312954d65ea41df9d20
sin wan wildy <sw.wildy@gmail.com>
2020-11-18 20:03:15 -0800

    done

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+    def initialize
+        @name
+        @breed
+    end
+
+    def name 
+        @name
+    end
+
+    def name=(str)
+        @name = str
+    end
+
+    def breed
+        @breed 
+    end 
+
+    def breed=(str)
+        @breed = str 
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+    def initialize 
+        @name
+        @job
+    end
+
+    def name 
+        @name
+    end
+
+    def name=(str)
+        @name = str
+    end 
+
+    def job
+        @job 
+    end
+
+    def job=(str)
+        @job = str
+    end
+
+end
